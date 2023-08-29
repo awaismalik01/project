b13a26a41983d8ccd53bb4813b0894e24271cd0d
awaismalik01 <mawais_malik@hotmail.com>
2023-08-29 02:59:42 -0700

    Added Semantic tags, Components and Meta tags

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React from "react";
+import Nav from "./JavaScript/Nav";
+import Header from "./JavaScript/Header";
+import Main from "./JavaScript/Main";
+import Footer from "./JavaScript/Footer";
+
 function App() {
-  
-  return <h1>Welcone</h1>;
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/JavaScript/Footer.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <img alt="footer" />
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
